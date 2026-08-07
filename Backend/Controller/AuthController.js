20d251944b855aa81ed1f99dddf3afb4a52ddc67
Muhammad Usman <abc.usman.100@gmail.com>
2026-08-07 15:07:05 +0500

    Logout Done

--- Backend/Controller/AuthController.js
+++ Backend/Controller/AuthController.js
@@ -241,4 +241,26 @@ const LoginUser = async (req, res) => {
   }
 };
 
-export { RegisterOwner, VerifyEmail, LoginUser };
+const LogoutUser = async (req, res) => {
+  try {
+    res.clearCookie('token', {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === 'production',
+      sameSite: 'strict',
+    });
+
+    return res.status(200).json({
+      success: true,
+      message: 'Logout successful',
+    });
+
+  } catch (err) {
+    console.error('Logout error:', err);
+    return res.status(500).json({
+      success: false,
+      message: 'Internal server error during logout',
+    });
+  }
+};
+
+export { RegisterOwner, VerifyEmail, LoginUser, LogoutUser };
--- Backend/Routers/userRouter.js
+++ Backend/Routers/userRouter.js
@@ -1,6 +1,6 @@
 import express from 'express';
 const router = express.Router();
-import {RegisterOwner, VerifyEmail, LoginUser} from '../Controller/AuthController.js'
+import {RegisterOwner, VerifyEmail, LoginUser, LogoutUser} from '../Controller/AuthController.js'
 import { RegisterValidator, loginValidator } from '../Validators/authValidator.js';
 import { validate } from '../Middlewares/validate.js';
 
@@ -8,6 +8,7 @@ import { validate } from '../Middlewares/validate.js';
 router.post('/register',RegisterValidator, validate, RegisterOwner);
 router.get('/verify-email', VerifyEmail)
 router.post('/login', loginValidator, validate, LoginUser);
+router.post('/logout',  LogoutUser);
 
 
 export default router; 
